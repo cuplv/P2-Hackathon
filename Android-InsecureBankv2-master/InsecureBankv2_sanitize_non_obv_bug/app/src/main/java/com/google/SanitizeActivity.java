a4e32f6c5d4c98d362ed2baa5f5a167d671527f4
Shawn Meier <shawn.meier@colorado.edu>
2017-05-11 19:50:07 -0400

    non obvious bug

---
+++ Android-InsecureBankv2-master/InsecureBankv2_sanitize_non_obv_bug/app/src/main/java/com/google/SanitizeActivity.java
@@ -0,0 +1,16 @@
+package com.google;
+
+import android.app.Activity;
+
+/**
+ * Created by s on 5/11/17.
+ */
+
+public class SanitizeActivity {
+    public static void sanitizeActivity(Activity activity){
+
+    }
+    public static void logActivity(Activity activity){
+
+    }
+}
